98442301dc8446ee1ec57bf6bef5b2c8b85384cc
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-09-29 18:39:12 +0800

    39 import important libaries

--- mcp-tool/main.py
+++ mcp-tool/main.py
@@ -3,6 +3,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+from mcp import (
+    ClientSession, # provide the framework for a Python app to act as an MCP client
+    StdioServerParameters, # a Pydantic class that specifies the parameters for the MCP server
+    )
+from mcp.client.stdio import stdio_client # spawn a new process, and communicate to the MCP server through stdio
+
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools # load the MCP tools from the server and transform them into LangChain tools
+from langgraph.prebuilt import create_react_agent
+
+
+llm = ChatOpenAI()
+
+
+# Pydantic object to specify the parameters for running the MCP server
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["C://Users//user//Desktop//learning_mcp//mcp-tool//servers//math_server.py"],
+)
+
+
 async def main():
     print("Hello from mcp-tool!")
 
