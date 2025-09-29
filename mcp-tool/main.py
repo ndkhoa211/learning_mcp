c6ce942dbabcdebe10f33663cf6ec57b68ee69c9
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-09-29 19:11:08 +0800

    41 create MCP client, initialize client session, and run MCP tools as LangChain tool, finally trace with LangSmith

--- mcp-tool/main.py
+++ mcp-tool/main.py
@@ -12,6 +12,7 @@ from mcp.client.stdio import stdio_client # spawn a new process, and communicate
 
 
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.tools import load_mcp_tools # load the MCP tools from the server and transform them into LangChain tools
 from langgraph.prebuilt import create_react_agent
 
@@ -27,7 +28,21 @@ stdio_server_params = StdioServerParameters(
 
 
 async def main():
-    print("Hello from mcp-tool!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("SESSION INITIALIZED")
+            # tools = await session.list_tools() # available tools from MCP SDK
+            # print(tools)
+            tools = await load_mcp_tools(session) # transform MCP tools into LangChain tools
+            # print(tools)
+            agent = create_react_agent(llm, tools)
+            result = await agent.ainvoke(
+                {
+                    "messages": [HumanMessage(content="What is 6*(5 + 2)?")]
+                }
+            )
+            print(result["messages"][-1].content)
 
 
 if __name__ == "__main__":
